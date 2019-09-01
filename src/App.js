fb46af7d8391b48843f5b3110e869c0889581b51
Shanthosh Krishnakumar <kkshanthosh@gmail.com>
2019-09-01 19:16:35 +1200

    App: Add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import Header from './components/Header';
 import './App.css';
 
 const App = () => {
-  return <div className="App">Test</div>;
+  return (
+    <div className="App">
+      <Header />
+      Test
+    </div>
+  );
 };
 
 export default App;
